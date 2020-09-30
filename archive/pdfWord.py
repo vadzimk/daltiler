4a79a0e8cbe2c10abf1023289afc54d6a97e3ee5
Vadzim <vadzimk@hotmail.com>
2020-09-29 17:41:17 -0700

    removed unnecessary folder

--- archive/pdfWord.py
+++
@@ -1,7 +0,0 @@
-import tabula
-
-# read pdf into data frame
-# df = tabula.read_pdf('test5.pdf', stream=True, pages='all')
-# print(df)
-
-tabula.convert_into('PRICEBOOK (Daltile).pdf', 'archive/all.csv', output_format='csv', pages='all', stream=True)
