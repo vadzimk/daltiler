22264ac3ba6032cb20ff937e4c56aa1f44bff677
Vadzim <vadzimk@hotmail.com>
2021-08-13 18:38:40 -0700

    change project structure

--- modules/PROJ_CONST.py
+++ modules/PROJ_CONST.py
@@ -1,12 +1,12 @@
-DIR_PROJECT = './project/'
-DIR_TABULATED_CSV = './project/tabulated_csv/'
-DIR_PRODUCT_TABLES = './project/dataframes/'
-DIR_TREATED_ROWS = './project/treatedrows/'
-DIR_XPDF = './project/html/'
+DIR_PROJECT = './project_daltiler/'
+DIR_TABULATED_CSV = './project_daltiler/tabulated_csv/'
+DIR_PRODUCT_TABLES = './project_daltiler/dataframes/'
+DIR_TREATED_ROWS = './project_daltiler/treatedrows/'
+DIR_XPDF = './project_daltiler/html/'
 
-DOC_PRODUCT_TABLE = './project/product_table.csv'
-DOC_TARGET = './project/target.csv'
-DOC_UOM = './project/uom.csv'
+DOC_PRODUCT_TABLE = './project_daltiler/product_table.csv'
+DOC_TARGET = './project_daltiler/target.csv'
+DOC_UOM = './project_daltiler/uom.csv'
 
 TARGET_CONFIG = './TARGET_CONFIG.csv'
 
